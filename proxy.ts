345c09d58840a29767dd3da40d499b48d6cea6ac
DeveloperShariful <sharifulislam78009@gmail.com>
2026-01-01 21:55:47 +0600

    geust checkout solve

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,8 @@
-// middleware.ts
+// proxy.ts
 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
+/*const isPublicRoute = createRouteMatcher([
   "/",
   "/shop(.*)",
   "/product(.*)",
@@ -16,11 +16,29 @@ const isPublicRoute = createRouteMatcher([
   "/api/webhooks(.*)"
 ]);
 
+
+
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
 });
+
+*/
+
+const isProtectedRoute = createRouteMatcher([
+  "/admin(.*)",     
+  "/profile(.*)",  
+  "/api/admin(.*)"   
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
+});
+
 
 export const config = {
   matcher: [

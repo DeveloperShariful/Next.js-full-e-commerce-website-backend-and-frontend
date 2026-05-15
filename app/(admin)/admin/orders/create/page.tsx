dbfe3894d8f85eb00e6301f4d9a5142a5e076bf9
DeveloperShariful <sharifulislam78009@gmail.com>
2026-05-16 02:47:44 +0600

    coupon woocommerce style update and create order

--- app/(admin)/admin/orders/create/page.tsx
+++ app/(admin)/admin/orders/create/page.tsx
@@ -127,7 +127,7 @@ export default function CreateOrderPage() {
   };
 
   return (
-    <div className="p-4 md:p-5 max-w-[100%] mx-auto min-h-screen bg-[#f0f0f1] text-[#3c434a] font-sans pb-20">
+    <div className="max-w-[100%] mx-auto min-h-screen bg-[#f0f0f1] text-[#3c434a] font-sans pb-20">
       
       {/* Top Header */}
       <div className="flex items-center gap-2 mb-4">
